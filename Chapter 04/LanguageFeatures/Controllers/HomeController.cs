067bd9706047e845448babdb3bacbf9319f1cc28
Steven Loh <steven.loh@gmail.com>
2016-02-24 17:05:21 +0800

    Object initializers.

--- Chapter 04/LanguageFeatures/Controllers/HomeController.cs
+++ Chapter 04/LanguageFeatures/Controllers/HomeController.cs
@@ -28,5 +28,34 @@ namespace LanguageFeatures.Controllers
             // generate the view
             return View("Result", (object)String.Format("Product name: {0}", productName));
         }
+
+        public ViewResult CreateProduct()
+        {
+            // create and populate a new Product object
+            Product myProduct = new Product
+            {
+                ProductID = 100,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Price = 275M,
+                Category = "Watersports"
+            };
+
+            return View("Result", (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+            Dictionary<string, int> myDict = new Dictionary<string, int>
+            {
+                { "apple", 10 },
+                { "orange", 20 },
+                { "plum", 30 }
+            };
+
+            return View("Result", (object)stringArray[1]);
+        }
     }
 }
