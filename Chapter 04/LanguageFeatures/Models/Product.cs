1639eb1675bbb5b22c8f8fb750c700023ac6a7cf
Steven Loh <steven.loh@gmail.com>
2016-02-24 16:35:52 +0800

    C# properties.

---
+++ Chapter 04/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        private string _name;
+        public string Name
+        {
+            get { return ProductID + _name; }
+            set { _name = value; }
+        }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
