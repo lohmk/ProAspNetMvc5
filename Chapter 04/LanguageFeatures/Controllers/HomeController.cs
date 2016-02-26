8ea2a1ebc4388f497f4d29378f7cb6dba0544bdc
Steven Loh <steven.loh@gmail.com>
2016-02-26 17:02:36 +0800

    LINQ.

--- Chapter 04/LanguageFeatures/Controllers/HomeController.cs
+++ Chapter 04/LanguageFeatures/Controllers/HomeController.cs
@@ -147,5 +147,33 @@ namespace LanguageFeatures.Controllers
 
             return View("Result", (object)result.ToString());
         }
+
+        public ViewResult FindProducts()
+        {
+            Product[] products =
+            {
+                new Product { Name = "Kayak", Category = "Watersports", Price = 275M },
+                new Product { Name = "Lifejacket", Category = "Watersports", Price = 48.95M },
+                new Product { Name = "Soccer ball", Category = "Soccer", Price = 19.50M },
+                new Product { Name = "Corner flag", Category = "Soccer", Price = 34.95M }
+            };
+
+            var foundProducts = from match in products
+                                orderby match.Price descending
+                                select new
+                                {
+                                    match.Name,
+                                    match.Price
+                                };
+
+            // create the result
+            StringBuilder result = new StringBuilder();
+            foreach (var p in foundProducts.Take(3))
+            {
+                result.AppendFormat("Price: {0} ", p.Price);
+            }
+
+            return View("Result", (object)result.ToString());
+        }
     }
 }
