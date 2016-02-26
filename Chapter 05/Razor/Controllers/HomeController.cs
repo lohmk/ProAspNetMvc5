df8d1dac86840bcf1787273492ca4d12eadff4c6
Steven Loh <steven.loh@gmail.com>
2016-02-26 23:49:24 +0800

    Setting attributes.

--- Chapter 05/Razor/Controllers/HomeController.cs
+++ Chapter 05/Razor/Controllers/HomeController.cs
@@ -27,5 +27,15 @@ namespace Razor.Controllers
         {
             return View(myProduct);
         }
+
+        public ActionResult DemoExpression()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
+            return View(myProduct);
+        }
     }
 }
