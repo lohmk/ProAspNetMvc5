ec7ccef28e9707263e61110adbd0bee94dfaf1d8
Steven Loh <steven.loh@gmail.com>
2016-03-03 13:47:16 +0800

    Abstract and mock repository for model Product.

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -29,7 +32,15 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            // put bindings here
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf board", Price = 179 },
+                new Product { Name = "Running shoes", Price = 95 }
+            });
+
+            kernel.Bind<IProductsRepository>().ToConstant(mock.Object);
         }
     }
 }
