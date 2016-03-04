418bc82c8d91713ec2cd7891c42448302cdbf9a5
Steven Loh <steven.loh@gmail.com>
2016-03-04 16:14:09 +0800

    Bugfix: Pagination for selected category.

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -176,5 +176,36 @@ namespace SportsStore.UnitTests
             // Assert
             Assert.AreEqual(categoryToSelect, result);
         }
+
+        [TestMethod]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+                new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
+                new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
+                new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
+                new Product { ProductID = 5, Name = "P5", Category = "Cat3" },
+            });
+
+            // Arrange - create a controller and make the page size 3 items
+            ProductController target = new ProductController(mock.Object);
+            target.PageSize = 3;
+
+            // Action - test the product counts for different categories
+            int res1 = ((ProductListViewModel)target.List("Cat1").Model).PagingInfo.TotalItems;
+            int res2 = ((ProductListViewModel)target.List("Cat2").Model).PagingInfo.TotalItems;
+            int res3 = ((ProductListViewModel)target.List("Cat3").Model).PagingInfo.TotalItems;
+            int resAll = ((ProductListViewModel)target.List(null).Model).PagingInfo.TotalItems;
+
+            // Assert
+            Assert.AreEqual(res1, 2);
+            Assert.AreEqual(res2, 2);
+            Assert.AreEqual(res3, 1);
+            Assert.AreEqual(resAll, 5);
+        }
     }
 }
--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -32,7 +32,9 @@ namespace SportsStore.WebUI.Controllers
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
+                    TotalItems = category == null ?
+                                 repository.Products.Count() :
+                                 repository.Products.Where(p => category == null || p.Category == category).Count()
                 },
                 CurrentCategory = category
             };
