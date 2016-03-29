307bbff68270378724ae5b4edd56e118ce54a3d4
Steven Loh <steven.loh@gmail.com>
2016-03-29 18:02:19 +0800

    Server side data validation using model data annotations.

--- SportsStore/SportsStore.WebUI/Global.asax.cs
+++ SportsStore/SportsStore.WebUI/Global.asax.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Infrastructure.Binders;
 
@@ -15,6 +17,9 @@ namespace SportsStore.WebUI
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            // Using database first approach, disable context initializer strategy.
+            Database.SetInitializer<EFDbContext>(null);
             ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
